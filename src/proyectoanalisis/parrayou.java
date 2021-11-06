c28d60f5f34e2fb0d6dc5609be912c13eadbb07e
carsangi <sanchez.carlos.5554@eam.edu.co>
2021-11-06 18:30:56 -0500

    parraYouAdd

---
+++ src/proyectoanalisis/parrayou.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoanalisis;
+
+/**
+ *
+ * @author Carlos
+ */
+public class parrayou {
+    public static void main(String[] args) {
+        System.out.println("proyectoanalisis.parrayou.main()");
+    }
+}
